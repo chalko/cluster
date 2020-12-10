cf2d57eb8a2e37e977fa82f8d1858c729ab99467
Nick J Chalko <nick@chalko.com>
2020-12-09 22:50:24 -0800

    Build latest k3os with latest released firmware

--- build-image.sh
+++ build-image.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 picl_generator_dir=~/src/picl-k3OS-image-generator/
 
+
 cp picl-config/*  ${picl_generator_dir}/config
 cd ~/src/picl-k3OS-image-generator/
-docker run -e TARGET=raspberrypi -v ${PWD}:/app -v /dev:/dev --privileged picl-builder:latest
-sudo chown nick picl-k3os-v0.11.0-raspberrypi.img
+docker run \
+  -e RASPBERRY_PI_FIRMWARE=1.20201126 \
+  -e K3OS_VERSION=v0.19.4-dev.5 \
+  -e TARGET=raspberrypi \
+  -v ${PWD}:/app \
+  -v /dev:/dev \
+  --privileged picl-builder:latest
+sudo chown nick picl-k3os-*.img

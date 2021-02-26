08ef6b2e0707c869446e7b7c8ea105f9799daa9d
Nick J Chalko <nick@chalko.com>
2021-02-25 19:59:19 -0800

    Exit if CD fails Update target k3s

--- build-image.sh
+++ build-image.sh
@@ -3,10 +3,10 @@ picl_generator_dir=~/src/picl-k3os-image-generator/
 
 
 cp picl-config/*  ${picl_generator_dir}/config
-cd ${picl_generator_dir}
+cd ${picl_generator_dir} || exit
 docker run \
   -e RASPBERRY_PI_FIRMWARE=1.20201126 \
-  -e K3OS_VERSION=v0.19.5-rc.1 \
+  -e K3OS_VERSION=v1.20.2+k3s1 \
   -e TARGET=raspberrypi \
   -v ${PWD}:/app \
   -v /dev:/dev \

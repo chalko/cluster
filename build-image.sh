5bb040dc1e01b1a299c0b7d15a9f041d30edfd7d
Nick J Chalko <nick@chalko.com>
2021-02-25 20:07:43 -0800

    update firmware to match raspios 2021-01-11

--- build-image.sh
+++ build-image.sh
@@ -5,7 +5,7 @@ picl_generator_dir=~/src/picl-k3os-image-generator/
 cp picl-config/*  ${picl_generator_dir}/config
 cd ${picl_generator_dir} || exit
 docker run \
-  -e RASPBERRY_PI_FIRMWARE=1.20201126 \
+  -e RASPBERRY_PI_FIRMWARE=1.20210104 \
   -e K3OS_VERSION=v1.20.2+k3s1 \
   -e TARGET=raspberrypi \
   -v ${PWD}:/app \

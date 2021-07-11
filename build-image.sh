ac30e3fc8ceacd1e3adfd08acecac6359cb3a0de
Nick J Chalko <nick@chalko.com>
2021-07-11 08:31:29 -0700

    Update both firmware and k3os

--- build-image.sh
+++ build-image.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 picl_generator_dir=~/src/picl-k3os-image-generator/
 
+# Firmware at https://github.com/raspberrypi/firmware/releases
+# K3OS at https://github.com/rancher/k3os/releases
+
 
 cp picl-config/*  ${picl_generator_dir}/config
 cd ${picl_generator_dir} || exit
 docker run \
-  -e RASPBERRY_PI_FIRMWARE=1.20210104 \
-  -e K3OS_VERSION="v0.19.5-rc.1" \
+  -e RASPBERRY_PI_FIRMWARE=1.20210527\
+  -e K3OS_VERSION="v0.20.7-k3s1r0" \
   -e TARGET=raspberrypi \
   -v ${PWD}:/app \
   -v /dev:/dev \
